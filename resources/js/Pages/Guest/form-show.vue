c585e36c55e0338f91f6a3abb7108d44f171efac
sam kordestani <mediastorertl@gmail.com>
2026-09-21 21:08:52 +0330

    Optimize dimensions on detail page images

--- resources/js/Pages/Guest/form-show.vue
+++ resources/js/Pages/Guest/form-show.vue
@@ -290,7 +290,7 @@ const submitReply = (id) => {
                                         <!-- MAIN SLIDES -->
                                         <div class="product-image-slider">
                                             <figure class="border-radius-10">
-                                                <img :src="$page.props.ziggy.url + '/storage/' + props.product.image.url" alt="product image" />
+                                                <img :src="$page.props.ziggy.url + '/storage/' + props.product.image.url" width="600" height="600" alt="product image" />
                                             </figure>
 
                                         </div>
