fd95421cc48eec4dc97c06f4892f4646b21d13e4
sam kordestani <mediastorertl@gmail.com>
2026-09-23 22:50:29 +0330

    fix: import Product model for sitemap

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Middleware\Check404;
 use App\Http\Middleware\Check503;
 use Illuminate\Support\Facades\Route;
 use App\Models\WebDesign;
+use App\Models\Product;
 use App\Models\Blog;
 use App\Http\Controllers\FaqController;
 use Illuminate\Support\Facades\Artisan;
