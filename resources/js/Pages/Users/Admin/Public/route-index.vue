3738634f2fd3a01685e94b7b8c4b4b747a45c78a
sam kordestani <mediastorertl@gmail.com>
2026-09-26 19:56:49 +0330

    fix: make admin action menus reliable

--- resources/js/Pages/Users/Admin/Public/route-index.vue
+++ resources/js/Pages/Users/Admin/Public/route-index.vue
@@ -37,6 +37,11 @@ const getPageUrl = (baseUrl, page) => {
 };
 
 
+const openMenu = ref(null);
+
+const toggleMenu = (id) => {
+    openMenu.value = openMenu.value === id ? null : id;
+};
 </script>
 <template>
     <Header :cart="props.cart" :cartPrice="props.cartPrice" :cartCount="props.cartCount" :cartDiscount="props.cartDiscount" :wallet="props.wallet"
@@ -99,8 +104,8 @@ const getPageUrl = (baseUrl, page) => {
 
                                                     <td class="text-end">
                                                         <div class="dropdown">
-                                                            <a href="#" data-bs-toggle="dropdown" class="btn btn-light rounded btn-sm font-sm"> <i class="material-icons md-more_horiz"></i> </a>
-                                                            <div class="dropdown-menu">
+                                                            <a href="#" @click.prevent.stop="toggleMenu(route.id)" class="btn btn-light rounded btn-sm font-sm"> <i class="material-icons md-more_horiz"></i> </a>
+                                                            <div v-if="openMenu === route.id" class="dropdown-menu show" @click.stop>
                                                                 <Link :href="route('route.show',[section.id])" class="dropdown-item">نمایش جزئیات</Link>
                                                                 <Link class="dropdown-item text-danger" :href="route('route.destroy',[section.id])" method="delete" as="button" :on-finish="submitTime">حذف</Link>
                                                             </div>
