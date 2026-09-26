d1d21ca41fa1466ac91e9ccc40f86e469d5adb55
sam kordestani <mediastorertl@gmail.com>
2026-09-26 20:19:17 +0330

    style: move admin create button left

--- resources/js/Pages/Users/Admin/Cafe-Net/cafe-net-index.vue
+++ resources/js/Pages/Users/Admin/Cafe-Net/cafe-net-index.vue
@@ -34,12 +34,12 @@ const toggleMenu = (id) => {
 <main class="main-wrap rtl">
         <section class="content-main">
             <div class="row content-header">
-                <div class="d-flex col-sm-12">
-                    <div class="content-title card-title" v-if="props.descriptions" v-html="props.descriptions.subject"></div>
-                    <td class="me-auto">
-                        <Link :href="route('cafe-net-admin.create')" class="btn btn-primary btn-sm rounded font-sm">ایجاد</Link>
-                    </td>
-                </div>
+                <div class="d-flex col-sm-12 align-items-center">
+                        <div class="content-title card-title" v-if="props.descriptions" v-html="props.descriptions.subject"></div>
+                    <div style="margin-right:auto; text-align:left;">
+                            <Link :href="route('cafe-net-admin.create')" class="btn btn-primary btn-sm rounded font-sm">ایجاد</Link>
+                        </div>
+                    </div>
                 <div class="col-sm-12">
                     <div v-if="props.descriptions" v-html="props.descriptions.text"></div>
                 </div>
