8c869ba0f5e08cb4ae740683ce0237ba2588a138
sam kordestani <mediastorertl@gmail.com>
2026-09-26 20:19:19 +0330

    style: move admin create button left

--- resources/js/Pages/Users/Admin/Public/Blog-index.vue
+++ resources/js/Pages/Users/Admin/Public/Blog-index.vue
@@ -43,11 +43,11 @@ const toggleMenu = (id) => {
         <main class="main-wrap rtl" >
             <section class="content-main">
                 <div class="row content-header">
-                    <div class="d-flex col-sm-12">
+                    <div class="d-flex col-sm-12 align-items-center">
                         <div class="content-title card-title" v-if="props.descriptions" v-html="props.descriptions.subject"></div>
-                        <td class="me-auto">
+                        <div style="margin-right:auto; text-align:left;">
                             <Link :href="route('blogAdmin.create')" class="btn btn-primary btn-sm rounded font-sm">ایجاد</Link>
-                        </td>
+                        </div>
                     </div>
                     <div class="col-sm-12">
                         <div v-if="props.descriptions" v-html="props.descriptions.text"></div>
