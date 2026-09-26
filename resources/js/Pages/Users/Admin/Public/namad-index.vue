0d5bd964fed391c332b7c0c11318d17c350a5cff
sam kordestani <mediastorertl@gmail.com>
2026-09-26 19:56:46 +0330

    fix: make admin action menus reliable

--- resources/js/Pages/Users/Admin/Public/namad-index.vue
+++ resources/js/Pages/Users/Admin/Public/namad-index.vue
@@ -124,6 +124,11 @@ const getPageUrl = (baseUrl, page) => {
     return `${baseUrl}?page=${page}`; // در محیط سرور فقط این URL بدون query string
 };
 
+const openMenu = ref(null);
+
+const toggleMenu = (id) => {
+    openMenu.value = openMenu.value === id ? null : id;
+};
 </script>
 <template>
 <Header :cart="props.cart"  :roles="props.roles" :alert="props.alert" :users="props.users" :wallet="props.wallet"
@@ -179,8 +184,8 @@ const getPageUrl = (baseUrl, page) => {
                                                             </td>
                                                             <td class="text-end">
                                                                 <div class="dropdown">
-                                                                    <a href="#" data-bs-toggle="dropdown" class="btn btn-light rounded btn-sm font-sm"> <i class="material-icons md-more_horiz"></i> </a>
-                                                                    <div class="dropdown-menu">
+                                                                    <a href="#" @click.prevent.stop="toggleMenu(namad.id)" class="btn btn-light rounded btn-sm font-sm"> <i class="material-icons md-more_horiz"></i> </a>
+                                                                    <div v-if="openMenu === namad.id" class="dropdown-menu show" @click.stop>
                                                                         <Link class="dropdown-item" :href="route('namadAdmin.show',[social.id])">ویرایش اطلاعات</Link>
                                                                         <Link class="dropdown-item text-danger" :href="route('namadAdmin.destroy',[social.id])" method="delete" as="button" :on-finish="submitTime">حذف</Link>
                                                                     </div>
